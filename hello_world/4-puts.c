d0382b54dbedaf56d46b40a7cae5057ed00c2bbd
PiusEzekiel <e.pius1@alustudent.com>
2024-05-11 00:52:55 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*comment
+/**
+ * comment
+ * 
  * second comment
- * */
+ */
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle.");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

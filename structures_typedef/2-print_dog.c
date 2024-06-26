cd98dc17f8c1973d4668f56d9f9df0a2b916be45
PiusEzekiel <e.pius1@alustudent.com>
2024-06-26 15:47:14 -0700

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: point to the dog struct
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d -- NULL)
+		return;
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
